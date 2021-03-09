883ae800553f083063e59825ed8d66778bc270b0
Daniel <danielkfosquerau@gmail.com>
2021-03-09 12:59:46 -0300

    App01_ConsultarCEP - Concluído

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using App01_ConsultarCEP.Model;
+using App01_ConsultarCEP.Service;
+using System;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnSend.Clicked += FindCep;
+
+            txtCEP.Completed += FindCep;
+        }
+
+        public void FindCep(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(txtCEP.Text))
+                DisplayAlert("Erro", "Campo vazio!", "Ok");
+            else
+            {
+                string cep = txtCEP.Text.Trim();
+                cep = cep.Replace("-", "");
+
+                if (IsValidCep(cep))
+                {
+                    try
+                    {
+                        Address address = ViaCepService.FindAddressByCep(cep);
+
+                        if (address != null)
+                            lblResult.Text = string.Format("Endereço: {0}, {1} - {2}, {3} - {4}, {5}",
+                                address.logradouro, address.complemento, address.bairro, address.localidade,
+                                address.uf, address.cep);
+                        else
+                            DisplayAlert("Erro", "Endereço não encontrado!", "OK");
+                    }
+                    catch (Exception e)
+                    {
+                        DisplayAlert("Erro", e.Message, "OK");
+                    }
+                }
+                else
+                {
+                    DisplayAlert("Erro", "Cep inválido!", "Ok");
+                }
+            }
+        }
+
+        private bool IsValidCep(string cep)
+        {
+            if (cep.Length != 8)
+                return false;
+
+            if (!int.TryParse(cep, out _))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ App01_ConsultarCEP/App01_ConsultarCEP/Service/ViaCepService.cs
@@ -0,0 +1,26 @@
+﻿using App01_ConsultarCEP.Model;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace App01_ConsultarCEP.Service
+{
+    public class ViaCepService
+    {
+        private static readonly string urlAddress = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Address FindAddressByCep(string cep)
+        {
+            string newUrlAddress = string.Format(urlAddress, cep);
+
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(newUrlAddress);
+
+            Address a = JsonConvert.DeserializeObject<Address>(content);
+
+            if (a.cep == null)
+                return null;
+
+            return a;
+        }
+    }
+}
